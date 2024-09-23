aa8d95e3f900af11dfc581125239c3cdc6529f1b
Jiujia <jiujia.zhang@gmail.com>
2024-09-22 21:02:29 -0400

    fix import

--- src/fit_model.py
+++ src/fit_model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from tools import load_json_with_arrays, ensure_dir_exists
+from src.tools import load_json_with_arrays, ensure_dir_exists
 import pickle
 
 def reparameterize(Ts, r=1, option=1):
